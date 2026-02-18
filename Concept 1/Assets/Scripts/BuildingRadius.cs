1a33f396ca82cfc99e6b68844104666b79505305
Bordocklius <miguel.dhaenens@hotmail.com>
2026-02-18 17:38:21 +0100

    kutzooi

---
+++ Concept 1/Assets/Scripts/BuildingRadius.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BuildingRadius : MonoBehaviour
+{
+    public float Radius;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, Radius);
+    }
+}

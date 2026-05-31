1d50abdc4f1f5b0b6281ad9b08a5c3b541682d05
Bordocklius <miguel.dhaenens@hotmail.com>
2026-05-31 14:30:41 +0200

    Updated controls to use moba esque controlls with holding the right mouse button

--- Concept 3/Assets/Scripts/PlayerMovement.cs
+++ Concept 3/Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,29 @@
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Settings")]
+    [Space(10), Header("InputActionReferences")]
+    [SerializeField] private InputActionReference _moveAction;
+    [SerializeField] private InputActionReference _mousePosAction;
+ 
+    [Space(10), Header("Settings")]
     [SerializeField] private CharacterController _charController;
     [SerializeField, Min(1f)] private float _movementSpeed = 5f;
 
 
     private Vector2 _movementInput;
     private float _verticalVelocity;
+    private bool _isHoldingMouse;
 
 
     [Space(10), Header("Visuals")]
     [SerializeField] private LayerMask _groundMask;
     [SerializeField] private Transform _visualRoot;
 
-    private Vector3 _previousMousePos = Vector3.zero;
+    private Vector3 _previousMousePos = Vector3.zero;    
 
     private void Awake()
     {
@@ -25,17 +31,29 @@ public class PlayerMovement : MonoBehaviour
             _charController = GetComponent<CharacterController>();
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void OnEnable()
     {
-        
+        _moveAction.action.performed += MoveAction_Performed;
+        _moveAction.action.canceled += MoveAction_Performed;
+    }    
+
+    private void OnDisable()
+    {
+        _moveAction.action.performed -= MoveAction_Performed;
+        _moveAction.action.canceled -= MoveAction_Performed;
     }
 
-    // Update is called once per frame
     void Update()
     {
+        Vector3 move = Vector3.zero;
         // Horizontal movement
-        Vector3 move = new Vector3(_movementInput.x, 0f, _movementInput.y);
+        if(_isHoldingMouse)
+        {
+            move = HandleMovementInput();
+        }
+
+        // Horizontal movement
+        //Vector3 move = new Vector3(_movementInput.x, 0f, _movementInput.y);
         HandleRotation(move);
 
         move = Vector3.ClampMagnitude(move, 1f); // avoid faster diagonal speed
@@ -51,6 +69,22 @@ public class PlayerMovement : MonoBehaviour
         move.y = _verticalVelocity;
 
         _charController.Move(move * Time.deltaTime);
+    }
+
+    private Vector3 HandleMovementInput()
+    {
+        Vector3 moveDir = Vector3.zero;
+        Vector3 mousePos = _mousePosAction.action.ReadValue<Vector2>();
+        Ray ray = Camera.main.ScreenPointToRay(mousePos);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, _groundMask, QueryTriggerInteraction.Ignore))
+        {
+            moveDir = (hit.point - transform.position);
+            moveDir.y = transform.position.y;
+            moveDir = moveDir.normalized;
+        }
+
+        return moveDir;
     }
 
     private void HandleRotation(Vector3 moveDirection)
@@ -73,11 +107,19 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnMove(InputValue inputValue)
+    private void MoveAction_Performed(InputAction.CallbackContext ctx)
     {
-        _movementInput = inputValue.Get<Vector2>();
+        if (ctx.performed)
+        {
+            Debug.Log("Holding mouse");
+            _isHoldingMouse = true;
+        }
+        else if (ctx.canceled)
+        {
+            Debug.Log("Holding stopped");
+            _isHoldingMouse = false;
+        }
     }
-
 
     public IEnumerator ApplySpeedBuff(float buffAmount, float duration)
     {
